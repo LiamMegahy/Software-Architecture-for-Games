876e8554f9ce60e6e7eec9abfadfb0685a70e6ce
LiamMegahy <72744951+LiamMegahy@users.noreply.github.com>
2022-01-25 15:49:25 +0000

    Create week.cpp

---
+++ week1/week.cpp
@@ -0,0 +1,146 @@
+// SAG_Week1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    //int age = 12;
+    //std::cout << "The variable contains the number : " << age * 2;
+
+    //int score;
+    //char firstName;
+    //char familyName; 
+    //std::cout << "Please input your highest defender score : ";
+    //std::cin >> score;
+    //std::cout << "Please input the first letter of your first name : ";
+    //std::cin >> firstName;
+    //std::cout << "Please input the first letter of your family name : ";
+    //std::cin >> familyName;
+    //std::cout << "Player " << firstName << "." << familyName << " highest score on defender is : " << score;
+
+    //int loss;
+    //std::cout << "There exists a number : " << loss;
+
+    //double example1 = 20000;
+    //float example2 = 30.125;
+    //int example3 = 20;
+    //char example4 = 'z';
+    //unsigned int example5 = -5;
+
+    //example3 = (float)example2;
+    //std::cout << "Float into a integer : " << example3;
+    //Outputs 30
+
+    //example1 = (float)example2;
+    //std::cout << "Float into a double : " << example1;
+    //Outputs 30.125
+
+    //example1 = (int)example3;
+    //std::cout << "Int into a double : " << example1;
+    //Outputs 20
+
+    //example3 = (char)example4;
+    //std::cout << "Character into an Int : " << example3;
+    //Outputs 122
+
+    //example4 = (int)example3;
+    //std::cout << "Integer into a char : " << example4;
+    //Outputs a emoticon sad face
+
+    /*int Age;
+    char Gender;
+    float Time; 
+    float Price = 6.00;
+    bool open = true;
+    bool allowed = true;
+
+    std::cout << "Please input your age : ";
+    std::cin >> Age;
+    std::cout << "Please input your gender : ";
+    std::cin >> Gender;
+    std::cout << "Please input the current time : ";
+    std::cin >> Time;
+
+        if (Time >= 17.00 || Time <= 4.00) {
+            open = true;
+
+            if (Time > 17.00 && Time <= 19.00) {
+                Price = Price;
+            }
+            else {
+                Price = 9.00;
+            }
+
+            if (Age >= 18 && Age <= 25) {
+                Price = Price - 2.00;
+            }
+
+            if (Time >= 19.00 && Time <= 20.00) {
+                if (Gender == 'f' || Gender == 'F') {
+                    Price = Price / 2;
+                }
+            }
+
+            if (Time >= 00.00 && Time < 04.00) {
+                if (Gender == 'm' || Gender == 'M') {
+                    Price = Price;
+                }
+            }
+            if (Gender != 'f' && Gender != 'F' && Gender != 'm' && Gender != 'M') {
+                std::cout << "Gender enterted inncorectly";
+                allowed = false;
+            }
+            else if (Age >= 18) {
+                if (open == true && allowed == true) {
+                    std::cout << "Your Price is : " << Price;
+                }
+            }
+            else if (Age < 18) {
+                std::cout << "You are not old enough to enter, age required is 18";
+            }
+
+        }  
+    else {
+        std::cout << "The club is closed, please come back later";
+    }
+    */
+
+//int i = 0;
+//while (i <= 12) {
+//    std::cout << i << "\n";
+//    i++;
+//}
+
+//int limit = 5000;
+//int load = 0;
+//int package = 0;
+//
+//while (load <= limit) {
+//    std::cout << "Enter the mass of the next package in KG : " << "\n";
+//    std::cin >> package;
+//    load = load + package;
+//    std::cout << "Current load = " << load << "\n";
+//}
+//std::cout << "Package too heavy " << "\n";
+
+//int numberOfGuesses = 0;
+//int randomNumber = (rand() % 100) + 1;
+//int userGuess = 0;
+//
+//while (userGuess != randomNumber) {
+//    std::cout << "Please guess a number : " << "\n";
+//    std::cin >> userGuess;
+//    numberOfGuesses++;
+//    if (userGuess < randomNumber) {
+//        std::cout << "You guessed a lower number" << "\n";
+//    }
+//    else {
+//        std::cout << "You guessed a higher number" << "\n";
+//    }
+//        std::cout << "Good guess!";
+//        std::cout << "That only took you " << numberOfGuesses << " guesses!";
+//}
+
+
+}
