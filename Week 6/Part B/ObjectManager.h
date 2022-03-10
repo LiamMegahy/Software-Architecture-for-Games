b0d8013bc29fca3e90d49bd333a164e21eea7f4a
LiamMegahy <72744951+LiamMegahy@users.noreply.github.com>
2022-03-10 16:24:44 +0000

    Add files via upload

---
+++ Week 6/Part B/ObjectManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "GameObject.h"
+#include <list>
+#include <string>
+
+class ObjectManager{
+public:
+	ObjectManager();
+	void UpdateAll(float frametime);
+	void RenderAll();
+	void AddObject(GameObject* pNewObject);
+	void deleteThis();
+	void deleteInactive();
+private:
+	std::list<GameObject*> GameObjectList;
+};
+
